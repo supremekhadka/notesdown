907e952c254262fa159ce67f41255dca808be2f3
Supreme Khadka <supremekhadka2@gmail.com>
2024-05-04 20:26:12 +0545

    minor style update

--- app/notes/page.tsx
+++ app/notes/page.tsx
@@ -1,7 +1,8 @@
 export default async function NotesPage() {
   return (
-    <div>
-		<h1>Notes</h1>
+    <div className="h-[40rem] flex flex-col items-center">
+		<h1 className="text-2xl mt-10 mb-48 font-semibold">Notes</h1>
+    <p>Notes are yet to be written ...</p>
 	</div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
   
 
   return (
-    <div className="h-[50rem] w-full dark:bg-black bg-white flex flex-col  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+    <div className="h-[40rem] w-full dark:bg-black bg-white flex flex-col  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
           {/* Radial gradient for the container to give a faded look */}
           <div className="absolute z-0 pointer-events-none inset-0 flex  items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
           <h1 className="text-6xl font-semibold">Scribblore ✍️</h1>
